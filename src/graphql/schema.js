9e9e5e941ba5e534174dfd3f6d71c3c7d8eae253
creativeBernadka <bfryczkowska@gmail.com>
2019-09-16 16:16:16 +0200

    ADD: schema for graphql added
    Schema changed for final version.

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,7 +2,25 @@ const {buildSchema} = require('graphql');
 
 const schema = buildSchema(`
     type Query {
-        message: String
+        history: [HistoryElement]
+        historyItem(id: Int!): HistoryElement
+    },
+    type HistoryElement {
+        id: Int
+        name: String
+        nickNames: [String]
+        payments: [Payment]
+        summary: [Settlement]
+    },
+    type Payment {
+        whoPayed: String!
+        forWhom: [String]
+        howMany: Float
+    },
+    type Settlement {
+        whoPays: String,
+        whomPays: String,
+        howMany: Float
     }
 `);
 
