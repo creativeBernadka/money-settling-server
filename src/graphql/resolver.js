5b5737e954d9c8af61f4bf29873bff091d73e0b8
creativeBernadka <bfryczkowska@gmail.com>
2019-09-16 17:13:37 +0200

    ADD: fake dataset added, resolvers adjusted
    Resolvers adjusted to fake dataset. The dataset will
    be removed in future versions and changed for MongoDB
    instance.

--- src/graphql/resolver.js
+++ src/graphql/resolver.js
@@ -1,5 +1,19 @@
+const data = require('./example-data');
+
+const getHistory = function (args) {
+  return data;
+};
+
+const getHistoryItem = function (args) {
+  const id = args.id;
+    return data.filter(item => {
+        return item.id === id;
+    })[0];
+};
+
 const root = {
-    message: () => 'Hello World'
+    history: getHistory,
+    historyItem: getHistoryItem
 };
 
 module.exports = root;
